d0e474c34a744b7c199a7f17de3aa7a194f1fee5
Regis Leandro da Silva Ribeiro Magalhães <engenheiro.regis.magalhaes@gmail.com>
2020-12-13 23:32:51 -0300

    Precisa concluir

---
+++ Classes/Elevador.cs
@@ -0,0 +1,32 @@
+namespace Elevador
+{
+    public class Elevador
+    {
+        public int andarAtual;
+        public int numAndares;
+        public int pessoas;
+        public float capacidade;
+
+        public string elevador(){
+
+            return "";
+
+        }
+
+        public string elevador(float capacidade, int andares){
+
+            return "";
+
+        }
+
+        public void adicionarPessoa(){
+
+        }
+
+        public void removerPessoa(){
+
+        }
+
+        
+    }
+}
